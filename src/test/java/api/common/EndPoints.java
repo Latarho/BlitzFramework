a4c2d33788c035fc533305b311773e1c8567bffe
Serg Pomytkin <latarho@gmail.com>
2022-05-25 14:41:37 +0300

    Update EndPoints.java

--- src/test/java/api/common/EndPoints.java
+++ src/test/java/api/common/EndPoints.java
@@ -12,19 +12,4 @@ public class EndPoints {
 
     // Courier - Delete Courier
     public static final String DELETE_COURIER = "/courier/";
-
-    // Order - Create Order
-    public static final String CREATE_ORDER = "/orders";
-
-    // Order - Cancel Order
-    public static final String CANCEL_ORDER = "/orders/cancel";
-
-    // Order - Accept Order
-    public static final String ACCEPT_ORDER = "/orders/accept/";
-
-    // Order - Get Orders List
-    public static final String GET_ORDERS = "/orders/";
-
-    // Order - Get Order By Track ID
-    public static final String GET_ORDER_BY_TRACK = "/orders/track";
 }
